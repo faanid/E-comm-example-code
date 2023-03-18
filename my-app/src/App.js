e3853230c26221baf84ffae3e7871b459d783224
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-03-19 00:44:05 +0330

    context state /  fix error

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
@@ -8,16 +8,15 @@ import Photos from "./pages/Photos";
 function App() {
   return (
     <div>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Photos />
-        </Route>
-
-        <Route path="/cart">
-          <Cart />
-        </Route>
-      </Switch>
+      <div>
+        <Header />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Photos />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,20 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
