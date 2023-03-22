646167493ae0376be0876f6e8b024cc308f4c909
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-03-22 23:27:03 +0430

    Custom Hover Hook

--- my-app/src/components/CartItem.js
+++ my-app/src/components/CartItem.js
@@ -1,8 +1,10 @@
 import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
 import { Context } from "../Context";
+import useHover from "../hooks/useHover";
 
 function CartItem({ item }) {
-  const [hovered, setHovered] = useState(false);
+  const [hovered, ref] = useHover();
   const { removeFromCart } = useContext(Context);
 
   const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
@@ -12,8 +14,7 @@ function CartItem({ item }) {
       <i
         className={iconClassName}
         onClick={() => removeFromCart(item.id)}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
+        ref={ref}
       ></i>
 
       <img src={item.url} width="130px" />
--- my-app/src/components/Imagas.js
+++ my-app/src/components/Imagas.js
@@ -2,9 +2,11 @@ import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
 
 import { Context } from "../Context";
+import useHover from "../hooks/useHover";
 
 function Image({ className, img }) {
-  const [hovered, setHovered] = useState(false);
+  const [hovered, ref] = useHover();
+  // const [hovered, setHovered] = useState(false);
   const { toggleFavorite, addToCart, cartItems, removeFromCart } =
     useContext(Context);
 
@@ -46,11 +48,7 @@ function Image({ className, img }) {
   }
 
   return (
-    <div
-      className={`${className} image-container`}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
+    <div className={`${className} image-container`} ref={ref}>
       <img src={img.url} className="image-grid" />
       {heartIcon()}
       {cartIcon()}
